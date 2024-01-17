10580b62046656e843420bb80fe473d3246640e3
rizzy <12hyojinkim14@gmail.com>
2024-01-17 23:48:56 +0900

    init

---
+++ src/app.controller.ts
@@ -0,0 +1,28 @@
+import { Controller, Get, Render } from '@nestjs/common';
+
+@Controller()
+export class AppController {
+    @Get()
+    @Render('main') // Render the 'main' EJS template
+    main() {
+        return { title: 'Home Page' }; // Pass data to the template
+    }
+
+    @Get('live')
+    @Render('livePage') // Render the 'main' EJS template
+    live() {
+        return { title: 'Live Page' }; // Pass data to the template
+    }
+
+    @Get('my-page')
+    @Render('channelInfo') // Render the 'main' EJS template
+    myInfo() {
+        return { title: 'My Page' }; // Pass data to the template
+    }
+
+    @Get('live-master')
+    @Render('live-provide-page') // Render the 'main' EJS template
+    provideLive() {
+        return { title: 'My Page' }; // Pass data to the template
+    }
+}

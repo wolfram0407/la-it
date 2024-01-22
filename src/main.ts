69b489b8db8e7ba299d0d7e9378c94ce68b88aed
sooyoung.choi <sooyoung.choi.dev@gmail.com>
2024-01-22 20:53:20 +0900

    Style - ejs 구조 변경 ver.1

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import { join } from 'path';
 
 async function bootstrap() {
     const app = await NestFactory.create<NestExpressApplication>(AppModule);
-    app.setGlobalPrefix('/api');
+    //app.setGlobalPrefix('/api');
     const config = new DocumentBuilder().setTitle('NestJS project').setDescription('').setVersion('1.0').addBearerAuth().build();
     const customOptions: SwaggerCustomOptions = {
         swaggerOptions: {

a94ff599e85d8424586411127fa6d01afaeea5a2
wolfram0407 <77099657+wolfram0407@users.noreply.github.com>
2024-01-24 17:39:05 +0900

    Feat - live 프론트 연결

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,19 +1,22 @@
 import { UserService } from './user/user.service';
 import { ApiTags } from '@nestjs/swagger';
-import { Controller, Get, Param, Render, Req } from '@nestjs/common';
+import { Controller, Get, Param, Render, Req, Res } from '@nestjs/common';
 import { LiveService } from './live/live.service';
 
 @ApiTags('Frontend')
 @Controller()
-export class AppController {
+export class AppController
+{
     constructor(
         private readonly userService: UserService,
         private readonly liveService: LiveService,
-    ) {}
+
+    ) { }
 
     @Get()
     @Render('main') // Render the 'main' EJS template
-    async main(@Req() req) {
+    async main(@Req() req)
+    {
         const lives = await this.liveService.findAll();
         console.log(lives);
         return { title: 'Home Page', path: req.url, lives: lives };
@@ -21,7 +24,8 @@ export class AppController {
 
     @Get('live/:liveId')
     @Render('main') // Render the 'main' EJS template
-    async live(@Param('liveId') liveId: string) {
+    async live(@Param('liveId') liveId: string, @Res() res: Response)
+    {
         const live = await this.liveService.findOne(+liveId);
         console.log(live);
         return { title: 'Live Page', path: '/live', live: live };
@@ -29,13 +33,18 @@ export class AppController {
 
     @Get('my-page')
     @Render('main') // Render the 'main' EJS template
-    myInfo(@Req() req) {
+    myInfo(@Req() req)
+    {
         return { title: 'My Page', path: req.url };
     }
 
-    @Get('live-master')
+
+    @Get('live-master/:channelId')
     @Render('main') // Render the 'main' EJS template
-    provideLive(@Req() req) {
-        return { title: 'live-master', path: req.url };
+    async provideLive(@Param('channelId') channelId: string, @Req() req)
+    {
+        const channel = await this.userService.FindChannelIdByChannel(+channelId);
+        console.log(channel);
+        return { title: 'live-master', path: '/live-master', channel };
     }
 }

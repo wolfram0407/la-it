986263cb9fdad6c95ffe14563688414e3c870a40
wolfram0407 <77099657+wolfram0407@users.noreply.github.com>
2024-01-27 19:36:36 +0900

    Fix - AccessToken 발급 시 Bearer 추가

--- src/auth/strategies/kakao.strategy.ts
+++ src/auth/strategies/kakao.strategy.ts
@@ -1,12 +1,12 @@
-import { UserService } from 'src/user/user.service';
-import { AuthService } from './../auth.service';
+import {UserService} from 'src/user/user.service';
+import {AuthService} from './../auth.service';
 
-import { Injectable, Res } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import { PassportStrategy } from '@nestjs/passport';
-import { Strategy, Profile } from 'passport-kakao';
+import {Injectable, Res} from '@nestjs/common';
+import {ConfigService} from '@nestjs/config';
+import {PassportStrategy} from '@nestjs/passport';
+import {Strategy, Profile} from 'passport-kakao';
 import * as _ from 'lodash';
-import { Response } from 'express';
+import {Response} from 'express';
 
 @Injectable()
 export class KakaoStrategy extends PassportStrategy(Strategy, 'kakao') {
@@ -14,31 +14,29 @@ export class KakaoStrategy extends PassportStrategy(Strategy, 'kakao') {
         private readonly configService: ConfigService,
         private readonly authService: AuthService,
         private readonly userService: UserService,
-    )
-    {
+    ) {
         super({
             clientID: configService.get<string>('KAKAO_KEY'),
             callbackURL: '/api/auth/login/kakao/callback',
         });
     }
-    async validate(accessToken: string, refreshToken: string, profile: Profile, done: any, @Res() res: Response): Promise<any>
-    {
+    async validate(accessToken: string, refreshToken: string, profile: Profile, done: any, @Res() res: Response): Promise<any> {
         const kakaoId = profile.id;
         const nickname = profile._json.properties.nickname;
         const profileImage = profile._json.properties.profile_image;
         const provider = profile.provider;
         let user = await this.authService.validateUser(kakaoId);
-        if (!user)
-        {
+        if (!user) {
             // 회원가입 진행
-            user = await this.userService.createUserAndChannel({ kakaoId, nickname, profileImage, provider });
+            user = await this.userService.createUserAndChannel({kakaoId, nickname, profileImage, provider});
             console.log(user);
         }
 
         const access_token = await this.authService.createAccessToken(user.userId);
-        console.log('access_token', access_token);
+        const Bearer = 'Bearer ' + access_token
+        console.log('access_token', Bearer);
         //const refresh_token = await this.authService.createRefreshToken(user.userId);
 
-        return { access_token };
+        return {access_token: Bearer};
     }
 }
